d0792ccd9b52da1fa9bde952ff12fecb513835be
sym <cukpupk>
2019-12-06 15:35:56 -0800

    Updates

---
+++ precache-manifest.6cbb6dca1f5f65c257032114392a1608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e18c6a191801af16cf887194baa996",
+    "url": "/react-tata/index.html"
+  },
+  {
+    "revision": "153f6c5b1d569e9cda93",
+    "url": "/react-tata/static/css/main.8feb0b6c.chunk.css"
+  },
+  {
+    "revision": "1328d6de331b72f58d33",
+    "url": "/react-tata/static/js/2.b5efd3cf.chunk.js"
+  },
+  {
+    "revision": "80b3222e1645a11eda6ff76f6a052141",
+    "url": "/react-tata/static/js/2.b5efd3cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "153f6c5b1d569e9cda93",
+    "url": "/react-tata/static/js/main.f37baa83.chunk.js"
+  },
+  {
+    "revision": "69f7477a1b78161c5aa8",
+    "url": "/react-tata/static/js/runtime-main.fc6a148f.js"
+  }
+]);
